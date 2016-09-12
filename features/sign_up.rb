0b928935a45aaa8f66ec5025ec259f107b029e48
Daniel Barreto <danielfsbarreto@gmail.com>
2016-09-12 20:36:29 -0300

    Sign up feature (pending)

---
+++ features/sign_up.rb
@@ -0,0 +1,11 @@
+Given(/^I access the book store$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I provide personal information to create a new account$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I successfully sign up$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end

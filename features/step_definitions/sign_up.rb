12227fc277f1747870d22457cb39feed010605a1
Daniel Barreto <danielfsbarreto@gmail.com>
2016-09-13 17:14:14 -0300

    Implement successful user sign up scenario

--- features/step_definitions/sign_up.rb
+++ features/step_definitions/sign_up.rb
@@ -3,9 +3,15 @@ Given(/^I access the book store$/) do
 end
 
 When(/^I provide personal information to create a new account$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  click_link('Sign up')
+  fill_in(id: 'user_username', with: 'Daniel')
+  fill_in(id: 'user_email', with: 'danielfsbarreto@gmail.com')
+  fill_in(id: 'user_password', with: '123123')
+  fill_in(id: 'user_password_confirmation', with: '123123')
+  click_button('Sign up')
 end
 
 Then(/^I successfully sign up$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  success_message = find('#flash_notice', text: 'Thank you for signing up! You are now logged in.')
+  expect(success_message).to be_visible
 end

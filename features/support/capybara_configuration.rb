f7f4a32199f2b78838d40c8b25c0fd7aaf8d4967
Daniel Barreto <daniel.barreto@toptal.com>
2016-12-08 16:22:32 -0300

    Switch of driver types

--- features/support/capybara_configuration.rb
+++
@@ -1,9 +0,0 @@
-require 'capybara/cucumber'
-require 'capybara/poltergeist'
-
-Capybara.configure do |config|
-  config.default_driver = :poltergeist
-  config.app_host = 'http://localhost:3000'
-  config.run_server = false
-  config.save_path = "#{Dir.pwd}/screenshots"
-end
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,1 +1,21 @@
+require 'capybara/cucumber'
+require 'capybara/poltergeist'
 require 'byebug'
+
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+end
+
+default_driver = case ENV['DRIVER']
+                 when 'headless'
+                   :poltergeist
+                 else
+                   :chrome
+                 end
+
+Capybara.configure do |config|
+  config.app_host = 'http://localhost:3000'
+  config.run_server = false
+  config.default_driver = default_driver
+  config.save_path = "#{Dir.pwd}/screenshots"
+end
